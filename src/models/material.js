07caec8b4626114624e8f26f7944304e6709f0c2
Dromedara <90615964+Dromedara@users.noreply.github.com>
2021-12-08 20:47:31 +0300

    Update material.js

--- src/models/material.js
+++ src/models/material.js
@@ -1,19 +1,11 @@
-//Schema Documentation: https://mongoosejs.com/docs/guide.html
 const {Schema, model} = require('mongoose');
 const {Types: {ObjectId}} = Schema;
-const Mat_Schema = new Schema(
+const materialSchema = new Schema(
     {
-        //
         value: {
             type: String,
             required: true,
             unique: true,
-        },
-        userId:{
-            //Это айди юзера или новости?
-            type: ObjectId,
-            ref: 'users',
-            required: true
         },
         title: {
             type: String,
@@ -32,4 +24,4 @@ const Mat_Schema = new Schema(
     }
 )
 
-module.exports = model('mtrl', Mat_Schema);
+module.exports = model('materials', materialSchema);
